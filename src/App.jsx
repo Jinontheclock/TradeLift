fbfe3b4703325dcc4fcef9d93e4c200127c0afaa
HAJIN LEE <kafka@HAJINs-MacBook-Pro.local>
2025-09-19 16:20:31 -0700

    hajin check in

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,19 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1>MDIA-3126-Digital Asset Integration</h1>
+    <h2>Lab#1: Codevase Setup</h2>
+    <h3>All team members should pull the changes and run the necessary installs. <br /> Please put your name on it and commit & push to the branch "MDIA3126_Lab01"</h3>
+    <ul>
+      <li>Hajin Lee check-in Sep 19th 4pm</li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
     </>
   )
 }
