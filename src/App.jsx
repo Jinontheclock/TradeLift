7b28a26788212a9ecc6fbde0535de2327031faa1
HidayatPatil <hpatil2@my.bcit.ca>
2025-09-21 00:47:53 -0700

    Hyde Check-in

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,11 @@ function App() {
   return (
     <>
     <h1>MDIA-3126-Digital Asset Integration</h1>
-    <h2>Lab#1: Codevase Setup</h2>
+    <h2>Lab#1: Codebase Setup</h2>
     <h3>All team members should pull the changes and run the necessary installs. <br /> Please put your name on it and commit & push to the branch "MDIA3126_Lab01"</h3>
     <ul>
       <li>Hajin Lee check-in Sep 19th 4pm</li>
-      <li></li>
+      <li>Hyde Patil check-in Sep 21th 12am</li>
       <li></li>
       <li></li>
       <li></li>
