7868219c2e71560d9b45356b51b2abad88ca38eb
HAJIN LEE <kafka@HAJINs-MacBook-Pro.local>
2025-09-29 01:24:38 -0700

    It's just for practice

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <>
+            <h1>MDIA-3126-Digital Asset Integration</h1>
+            <h2>Lab#1: Codebase Setup</h2>
+            <h3>
+                All team members should pull the changes and run the necessary
+                installs. <br /> Please put your name on it and commit & push to
+                the branch "MDIA3126_Lab01"
+            </h3>
+            <ul>
+                <li>Hajin Lee check-in Sep 19th 4pm</li>
+                <li>Hyde Patil check-in Sep 21th 12am</li>
+                <li>Leana Le check-in Sep 21th 1am</li>
+                <li>Luca Calamo check-in Sep 21st 2pm </li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </>
+    );
+}
+
+export default App;
